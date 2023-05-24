840c1a412355c7351063533d92cddb25ee4eaa64
Deo Valiandro. M <valiandrod@gmail.com>
2023-05-24 11:59:31 +0800

    init git, add entity and repository
    Signed-off-by: Deo Valiandro. M <valiandrod@gmail.com>

---
+++ Entity/Todolist.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Entity {
+    class Todolist {
+        private string $todo;
+
+        public function getTodo(): string
+        {
+            return $this->todo;
+        }
+
+        public function setTodo(string $todo): void
+        {
+            $this->todo = $todo;
+        }
+
+
+    }
+}
