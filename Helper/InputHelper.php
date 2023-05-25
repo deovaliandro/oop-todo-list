88cc3994ac9acd9c88025bde57675726ac78fbd3
deovaliandro <valiandrod@gmail.com>
2023-05-25 11:42:04 +0800

    add input helper
    Signed-off-by: deovaliandro <valiandrod@gmail.com>

---
+++ Helper/InputHelper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Helper {
+    class InputHelper
+    {
+        static function input(string $info): string
+        {
+            echo "$info :";
+            return trim(fgets(STDIN));
+        }
+    }
+}
